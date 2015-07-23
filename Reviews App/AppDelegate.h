ba3a1ad980262c094e2c7b84cc3c59db22453cf5
carlospinto13 <capv_pintoval@hotmail.com>
2015-07-23 15:02:09 -0400

    Initial Commit

---
+++ Reviews App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reviews App
+//
+//  Created by Carlos Pinto on 7/23/15.
+//  Copyright (c) 2015 cs2680. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reviews App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Reviews App
+//
+//  Created by Carlos Pinto on 7/23/15.
+//  Copyright (c) 2015 cs2680. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
